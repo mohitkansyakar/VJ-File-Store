b774791d2466bf440aa4efb10fc21ac6c5c4ec24
mohitkansyakar <mohitkansyakar36@gmail.com>
2025-04-29 19:45:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "mongodb+srv://mohitkansyakar36:BWvLlqHKHPi16e1r@cluster0.hiqn1ax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "mohitkansyakar36")
+DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', False)) # Set True or False
