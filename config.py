a8ba9de5c22c8d5ab4fda3613178bc098e75b6d6
mohitkansyakar <mohitkansyakar36@gmail.com>
2025-05-07 15:58:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002347518970"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002568465368"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
@@ -56,11 +56,11 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "shortner.in") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "18df48ba06d2cdeafe0ccc5dc7d7093950bb23eb") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "linkcents.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "882543d99bd932ae4013c3e5c1a35d9aaf4fd8c9") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
